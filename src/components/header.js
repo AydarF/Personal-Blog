0781d06490304bd06cc10c8696ffaea6225f1fb9
AydarF <aydar_fayzullin@yahoo.com>
2022-08-15 18:42:50 -0400

    refactor: hide navigation links

--- src/components/header.js
+++ src/components/header.js
@@ -2,51 +2,57 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
+// import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
 const NavLink = styled(Link)`
-    color: #222;
-    font-size: 1rem;
-    font-weight: ${props => props.fontWeight || 'normal'};
-    line-height: 1;
-    margin: 0 0.5rem 0 0;
-    padding: 0.25rem;
-    text-decoration: none;
+  color: #222;
+  font-size: 1rem;
+  font-weight: ${props => props.fontWeight || 'normal'};
+  line-height: 1;
+  margin: 0 0.5rem 0 0;
+  padding: 0.25rem;
+  text-decoration: none;
 
-    &.current-page {
-        border-bottom: 2px solid #222;
-    }
+  &.current-page {
+    border-bottom: 2px solid #222;
+  }
 
-    &:last-of-type {
-        margin-right: 0;
-    }
-`
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
 
 const Header = () => (
-        <header
-            css={css`
-                display: flex;
-                justify-content: space-between;
-                padding: 0.5rem 5vw;
-                background: #eee;
-                border-bottom: 1px solid #ddd;
+  <header
+    css={css`
+      display: flex;
+      justify-content: space-between;
+      padding: 0.5rem 5vw;
+      background: #eee;
+      border-bottom: 1px solid #ddd;
 
-                @media (min-width: calc(550px + 10vw)) {
-                    padding-left: calc((100vw - 550px - 0.5rem) / 2);
-                    padding-right: calc((100vw - 550px - 0.5rem) / 2);
-                }
-            `}>
-            <NavLink to="/" fontWeight="bold">Aydar's Blog</NavLink>
-            <nav css={css`
-                margin-top: 0;
-            `}>
-                <NavLink to="/" activeClassName="current-page">
-                    Home
-                </NavLink>
-                <NavLink to="/about/" activeClassName="current-page">
-                    About
-                </NavLink>
-            </nav>
-        </header>
-)
+      @media (min-width: calc(550px + 10vw)) {
+        padding-left: calc((100vw - 550px - 0.5rem) / 2);
+        padding-right: calc((100vw - 550px - 0.5rem) / 2);
+      }
+    `}
+  >
+    <NavLink to="/" fontWeight="bold">
+      Aydar's Blog
+    </NavLink>
+    {/*<nav
+      css={css`
+        margin-top: 0;
+      `}
+    >
+      <NavLink to="/" activeClassName="current-page">
+        Home
+      </NavLink>
+      <NavLink to="/about/" activeClassName="current-page">
+        About
+      </NavLink>
+    </nav>*/}
+  </header>
+);
 
 export default Header;
